6d89aeccb0324cf5721cd135e292da64f5944f6b
Yibo <wmxc1214@gmail.com>
2015-02-15 13:41:00 +0000

    Yibo Afternoon

---
+++ Assets/Platform/Basic/TextureChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureChange : MonoBehaviour {
+	public Texture[] mainTexture;
+	public Shader Script_shader = Shader.Find("Transparent/Diffuse");
+	// Use this for initialization
+	void Start () {
+		if (mainTexture.Length > 0) {
+			renderer.material.SetTexture ("_MainTex", mainTexture [pick_random_texture ()]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private int pick_random_texture(){
+		return Random.Range(0, this.mainTexture.Length);
+	}
+}
--- Assets/Scripts/Level_basics.cs
+++ Assets/Scripts/Level_basics.cs
@@ -2,39 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Level_basics : MonoBehaviour {
+public class Level_basics : MonoBehaviour
+{
 	
 	public Transform prefab;
 	public int numberOfObjects;
-
 	public float lengthOffset;
 	public float recycleOffset;
-
 	public Vector3 startPosition;
-	
-
 	private Vector3 nextPosition;
 	private Queue<Transform> objectQueue;
-    
 
-	void Start () {
-		objectQueue = new Queue<Transform>(numberOfObjects);
-		for(int i = 0; i < numberOfObjects; i++){
-			objectQueue.Enqueue((Transform)Instantiate(prefab));
+	void Start ()
+	{
+		objectQueue = new Queue<Transform> (numberOfObjects);
+		for (int i = 0; i < numberOfObjects; i++) {
+			objectQueue.Enqueue ((Transform)Instantiate (prefab));
 		}
 		nextPosition = startPosition;
-		for(int i = 0; i < numberOfObjects; i++){
-			Recycle();
+		for (int i = 0; i < numberOfObjects; i++) {
+			Recycle ();
 		}
 	}
 	
-	void Update () {
-		if(objectQueue.Peek().localPosition.z + recycleOffset < PlayerControl.distanceTraveled){
-			Recycle();
+	void Update ()
+	{
+		try {
+			if (objectQueue.Peek ().localPosition.z + recycleOffset < PlayerControl.distanceTraveled) {
+				Recycle ();
+			}
+		} finally {
+
 		}
 	}
 	
-	private void Recycle () {
+	private void Recycle ()
+	{
 		Vector3 scale = new Vector3(
 			1,
 			1,
@@ -43,15 +46,15 @@ public class Level_basics : MonoBehaviour {
 		Vector3 position = nextPosition;
 		
 		
-		Transform o = objectQueue.Dequeue();
+		Transform o = objectQueue.Dequeue ();
 		o.localScale = scale;
 		o.localPosition = position;
-		objectQueue.Enqueue(o);
+		objectQueue.Enqueue (o);
 		
-		nextPosition += new Vector3(
+		nextPosition += new Vector3 (
 			0,
 			0,
-			scale.z);
+			lengthOffset);
 		
 	}
 }
