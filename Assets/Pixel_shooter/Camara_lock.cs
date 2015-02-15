ddf8fa52c8d47d2a9f16d44add7ab5d5388c0325
Yibo <wmxc1214@gmail.com>
2015-02-15 07:02:29 +0000

    Shooter_game_merge

---
+++ Assets/Pixel_shooter/Camara_lock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara_lock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject shooter = GameObject.Find ("Shooter");
+		transform.position= new Vector3(shooter.transform.position.x, shooter.transform.position.y,-20 );
+	}
+}
---
+++ Assets/Pixel_shooter/Walls/RoomScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomScript : MonoBehaviour {
+
+	public GameObject room_prefab;
+	public int level_number;
+	public bool door_enabled;
+	// Use this for initialization
+	void Start () {
+		int start_i = 0 - level_number +1;
+		int end_i =0+level_number -1;
+		int i;
+		int j;
+		for (i=start_i; i<=end_i; i++) {
+			for (j=start_i; j<=end_i; j++) {
+				Instantiate(room_prefab, new Vector3(i*30,j*30,0),Quaternion.identity);
+
+			}
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
